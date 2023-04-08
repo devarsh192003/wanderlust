16b770a47116739a3c5bbc3ac08e0a58aec4e1ac
devarsh192003 <devarshpatel197@gmail.com>
2023-04-08 16:52:53 +0530

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,24 @@
 import React from 'react';
-
+import Back from '../components/Back';
+import Heading from '../components/Heading';
+import img from '../assets/img1.png'
 const About = () => {
 return (
-	<div
-	style={{
-		display: 'flex',
-		
-		height: '100vh'
-	}}
-	>
-	<h1>Wanderlust is a travel package provider</h1>
-	</div>
+	<>
+      <section className='about'>
+        <Back name='About Us' title='About Us - Who We Are?' cover={img} />
+        <div className='container flex mtop'>
+          <div className='left row'>
+            <Heading title='Our Agency Story' subtitle='Check out our company story and work process' />
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+            <button className='btn4'>More About Us</button>
+          </div>
+          
+        </div>
+      </section>
+    </>
 );
 };
 
